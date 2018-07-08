7ef43354a6b0215d0c76e2760cfe20fdf123e504
esgariot88 <esgariot@gmail.com>
2018-07-09 01:44:10 +0700

    Первое домашнее задание.

---
+++ Задание№1(easy).py
@@ -0,0 +1,28 @@
+# Задача №1
+
+Greeting = "Привет,Мир!"
+print(Greeting)
+
+Greeting = "Привет,Мир питона!"
+print(Greeting)
+
+a=10
+b=20
+c=30
+
+print(a, b, c)
+
+# Задача №2
+
+action = int(input("Введите число:"))
+
+print(action +2)
+
+# Задача №3
+
+age_limit = 18
+age=int(input("Введите ваш возраст:"))
+if age == age_limit:
+    print("Доступ разрешен")
+if age != age_limit:
+    print("Доступ запрещен")
